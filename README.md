91ce29e179006c059e35e43704a05705ea36d17e
丁宇 <jlusdy@gmail.com>
2012-07-19 16:21:06 +0800

    提交初始版本

--- README.md
+++ README.md
@@ -2,3 +2,50 @@ TBJMap
 ======
 
 JMap Enhanced Edition
+
+基于Serviceability Agent,对JMap做了增强,可以方便的输出JVM堆中,每一个分区的对象实例个数和大小的Histogram图
+
+使用方法:
+
+`java -classpath /home/admin/tbjmap.jar:/java/lib/sa-jdi.jar sun.jvm.hotspot.tools.TBJMap -histo PID > jmap_output.log`
+`java -classpath /home/admin/tbjmap.jar:/java/lib/sa-jdi.jar sun.jvm.hotspot.tools.TBJMap -oldhisto PID > jmap_oldoutput.log`
+
+执行完毕会输出如下的信息:
+> 
+> Old Object Histogram:
+> 
+> num	all instances/bytes	old instances/bytes	Class description
+> -----------------------------------------------------------------------------------
+> 1:	1831111/70874072	1798386/68395648	char[]
+
+> 2:	1871467/59886944	1840369/58891808	java.lang.String
+
+> 3:	1224464/39182848	1224384/39180288	java.util.HashMap$Entry
+
+> 4:	188390/26556456	188317/26547800	java.util.HashMap$Entry[]
+
+> 5:	395298/25299072	395298/25299072	forest.dataobject.impl.DefaultPropertyValueDO
+
+> 6:	1046319/25111656	1046181/25108344	java.lang.Long
+
+> 7:	332206/23910336	329739/23821816	java.lang.Object[]
+
+> 8:	117127/19404056	88705/15361808	long[]
+
+> 9:	147064/17362472	134052/15619640	byte[]
+
+> 10:	156284/17022448	136050/15396256	int[]
+
+> 11:	412608/16504320	412402/16496080	java.util.LinkedHashMap$Entry
+
+> 12:	325472/10415104	325472/10415104	forest.dataobject.impl.DefaultValueDO
+
+> 13:	304452/9742464	304452/9742464	forest.dataobject.impl.FeatureDO
+
+> 14:	190344/9136512	190344/9136512	forest.dataobject.impl.IntegerMapSupportArrayList
+
+> 15:	188028/9025344	187971/9022608	java.util.HashMap
+
+> 16:	88013/7041040	88013/7041040	forest.dataobject.impl.FileStdCategoryPropertyDO
+
+**线上使用建议先offline应用**
---
+++ src/sun/jvm/hotspot/oops/TBObjectHistogram.java
@@ -0,0 +1,90 @@
+package sun.jvm.hotspot.oops;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+public class TBObjectHistogram implements HeapVisitor {
+
+	private HashMap<Klass, TBObjectHistogramElement> map;
+
+	public TBObjectHistogram() {
+		map = new HashMap<Klass, TBObjectHistogramElement>();
+	}
+
+	public void prologue(long size) {
+	}
+
+	public boolean doObj(Oop obj) {
+		Klass klass = obj.getKlass();
+		if (!map.containsKey(klass))
+			map.put(klass, new TBObjectHistogramElement(klass));
+		((TBObjectHistogramElement) map.get(klass)).updateWith(obj);
+		return false;
+	}
+
+	public void epilogue() {
+	}
+
+	/**
+	 * Call this after the iteration is complete to obtain the
+	 * ObjectHistogramElements in descending order of total heap size consumed
+	 * in the form of a List<ObjectHistogramElement>.
+	 */
+	public List<TBObjectHistogramElement> getElements() {
+		List<TBObjectHistogramElement> list = new ArrayList<TBObjectHistogramElement>();
+		list.addAll(map.values());
+		Collections.sort(list, new Comparator<TBObjectHistogramElement>() {
+			public int compare(TBObjectHistogramElement o1, TBObjectHistogramElement o2) {
+				return o1.compare(o2);
+			}
+		});
+		return list;
+	}
+
+	public void print() {
+		printOn(System.out);
+	}
+
+	public void printOn(PrintStream tty) {
+		List<TBObjectHistogramElement> list = getElements();
+		TBObjectHistogramElement.titleOn(tty);
+		Iterator<TBObjectHistogramElement> iterator = list.listIterator();
+		int num = 0;
+		int totalCount = 0;
+		int totalSize = 0;
+		while (iterator.hasNext()) {
+			TBObjectHistogramElement el = iterator.next();
+			num++;
+			totalCount += el.getCount();
+			totalSize += el.getSize();
+			tty.print(num + ":" + "\t");
+			el.printOn(tty);
+		}
+		tty.println("Total : " + "\t" + totalCount + "\t" + totalSize);
+	}
+
+	public void printOnOld(PrintStream tty) {
+		List<TBObjectHistogramElement> list = getElements();
+		TBObjectHistogramElement.titleOnOld(tty);
+		Iterator<TBObjectHistogramElement> iterator = list.listIterator();
+		int num = 0;
+		int totalOldCount = 0;
+		int totalOldSize = 0;
+		while (iterator.hasNext()) {
+			TBObjectHistogramElement el = iterator.next();
+			if (el.getOldCount() > 0) {
+				totalOldCount += el.getOldCount();
+				totalOldSize += el.getOldSize();
+				num++;
+				tty.print(num + ":" + "\t");
+				el.printOnOld(tty);
+			}
+		}
+		tty.println("Total : " + "\t" + totalOldCount + "\t" + totalOldSize);
+	}
+}
